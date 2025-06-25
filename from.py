1ed552ed44aee569b1d523079cc524f4fbb82680
Niloofar.Mehrzad <126786468+Niloofar-Mehrzad@users.noreply.github.com>
2025-06-25 17:23:07 +0200

    Update path

--- from.py
+++ from.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import tarfile
 
 # Path to your tar.gz file
-tar_path = "C:/Users/mmehr/OneDrive/Desktop/CHALMERS/3_ThirdYear/3_StudyPeriod2/Introduction to data science and AI - DAT565/Assignments/Assignment_2/directory_kungalv_slutpriser/kungalv_slutpriser.tar"
-extract_path = "C:/Users/mmehr/OneDrive/Desktop/CHALMERS/3_ThirdYear/3_StudyPeriod2/Introduction to data science and AI - DAT565/Assignments/Assignment_2"
+tar_path = "C:/Users/directory_kungalv_slutpriser/kungalv_slutpriser.tar"
+extract_path = "C:/Users/extract"
 
 with tarfile.open(tar_path, "r") as tar:
     tar.extractall(path=extract_path)
